20e153a485875b06ec2b7f270c18825685ea0fc0
Allan <162851663+githuballan@users.noreply.github.com>
2024-05-16 14:40:55 -0300

    Módulo java script iniciado

---
+++ modulo-05/capitulo-29/script.js
@@ -0,0 +1,26 @@
+var a = 30;
+var b = 5;
+var c = 'joão';
+console.log(a+b+c+a+b);
+
+a="30";
+
+b="5";
+Number.parseInt(b)
+b+=b
+var c = Number.parseInt(a)+Number.parseInt(b)
+ 
+
+console.log(a)
+console.log(a==b?"sim":"Não")
+
+console.log(b)
+
+console.log(c)
+
+a=b
+
+if(a==b) {c=1}
+    else{c=2}
+    
+console.log(c)
