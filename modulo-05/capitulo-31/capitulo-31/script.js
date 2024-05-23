c9442d30b140187070c7935d4a6cc8dc12b07e5d
Allan <162851663+githuballan@users.noreply.github.com>
2024-05-22 23:12:02 -0300

    Início de array

--- modulo-05/capitulo-31/capitulo-31/script.js
+++ modulo-05/capitulo-31/capitulo-31/script.js
@@ -1,2 +1,39 @@
 familia = new Array("Allan","Karol","Maya","Moana");
-console.log(familia[4-1]);
+console.log(familia[3]);
+
+for(var membro in familia){
+    console.log(membro);
+}
+
+let frutas = ['maçã', 'banana', 'laranja'];
+console.log(frutas[1]);
+
+console.log(frutas.push('Morango'));
+console.log(frutas.pop());
+console.log(frutas.length);
+
+let indice = frutas.indexOf('banana');
+console.log(indice);
+
+console.log(frutas.shift());
+console.log(frutas.unshift('Uva'));
+
+
+frutas.splice(1, 0, "maçã", "bolo", "refrigerante");
+
+
+
+
+frutas.forEach(function(item, indice) {
+    console.log(item, indice);
+  });
+  console.log("------------------------------------------------------")
+
+  frutas.splice(2, 2);
+
+  frutas.forEach(function(item, indice) {
+    console.log(item, indice);
+  });
+
+  let novoArray = frutas.slice(3, 4);
+  console.log(novoArray)
