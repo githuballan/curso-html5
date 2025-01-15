2446fefc3cd0d7ccd1b4e7d5cb06163f36dc192d
Allan <162851663+githuballan@users.noreply.github.com>
2025-01-15 12:13:01 -0300

    Criação da Rifa online

--- modulo-06/capitulo-34/script.js
+++ modulo-06/capitulo-34/script.js
@@ -13,70 +13,69 @@ p.textContent="textContent";}
 {let p = document.getElementById("BCont2")
     console.log(p.textContent);}
 
-    function modgm(){
-        let p = document.getElementById("lafeminino")
-        p.textContent='Masculino:';
-        p.style.marginRight='2px';
-    }
 
-    function modhm(){
-        let p = document.getElementById("lamasculino")
-        p.textContent='Feminino:';
-        p.style.marginRight='9px';
-           
-    }
-    function modgo(){
-        let p = document.getElementById("lafeminino")
-        p.textContent='Feminino:';
-        p.style.marginRight='9px';
-        
-    }
+function mudar(m){
+    if(m==1){
+    let e = document.getElementById("hete")
+    let o = document.getElementById("homo")
+    e.style.marginRight='6px';
+    o.style.marginRight='0px';
 
-    function modho(){
-        let p = document.getElementById("lamasculino")
-        p.textContent='Masculino:';
-        p.style.marginRight='2px';
-        
-    }
+    e.innerHTML="Homosexual:";
+    o.innerHTML="Heterosexual:"
+}else{
+    let e = document.getElementById("hete")
+    let o = document.getElementById("homo")
+    o.style.marginRight='7px';
+    e.style.marginRight='0px';
 
-    {
-        function modoe(){
-            let p = document.getElementById("lahomo")
-            p.textContent='Héterosexual:';
-            p.style.marginRight='2px';
-        }
-    
-        function modeo(){
-            let p = document.getElementById("lahete")
-            p.textContent='Homosexual:';
-            p.style.marginRight='9px';
-               
-        }
-          
-        function modoed(){
-            let p = document.getElementById("lahete")
-            p.textContent='Héterosexual:';
-            p.style.marginRight='2px';
-        }
-    
-        function modeod(){
-            let p = document.getElementById("lahomo")
-            p.textContent='Homosexual:';
-            p.style.marginRight='9px';
-               
-        }
-        
+    e.innerHTML="Heterosexual:";
+    o.innerHTML="Homosexual:"
+
+}
+}
+
+function troca(t){
+    if(t==1){
+    let f = document.getElementById("lafeminino")
+    let m = document.getElementById("lamasculino")
+    f.textContent='Masculino:';
+    m.textContent='Feminino:';
+
+    f.style.marginRight='0px';
+    m.style.marginRight='7px';
+    }else{
+    let f = document.getElementById("lafeminino")
+    let m = document.getElementById("lamasculino")
+    m.textContent='Masculino:';
+    f.textContent='Feminino:';
+
+    f.style.marginRight='7px';
+    m.style.marginRight='0px';
+
     }
-        function idade(){
+}
+
+ function idade(){
         let p = document.getElementById("lidade")
         let result = document.getElementById("resultado")
         let n= Number.parseInt(p.value);
         let r = n+5;
         result.innerHTML =r;
 
-    }
+}
 
+var parente = "filho2";
+ var f = "parentElement";
+
+function pai(){
    
+
+    p=document.getElementById(parente);
+    let r = document.getElementById("resposta");
+    r.innerHTML=p.parentElement;
+    console.log(p.parentElement);
+}
 
    
         
---
+++ teste/rifa/script.js
@@ -0,0 +1,94 @@
+var nome = [];
+var nr = 0;
+var nrv=0;
+var nomev = [];
+
+
+onload = function () { 
+    nomev = JSON.parse(localStorage.getItem('nomev'));
+    nome = JSON.parse(localStorage.getItem('nome'));
+    if (nome == null) {
+        nome = [];
+    }
+    console.log(nome);
+    console.log(nomev);
+   
+}
+
+var cont=0;
+
+function sorteio() {
+    dis=document.getElementById('res');
+    console.log(dis);
+    dis.style.display = "block";
+
+    var n = Math.floor(Math.random() * nomev.length);
+    inde= nome.indexOf(nomev[n]);
+    if(n==0){n++;}
+ 
+    console.log(inde);
+   console.log(nomev[n]);
+    console.log(n);
+    document.getElementById('nomes').innerHTML ="Parabéns "+ nomev[n]+"<br>" + "O número sorteado foi: "+inde;
+    
+    
+    
+}
+function add() {
+    var nm = document.getElementById('nomeRifa').value;
+    let nr = document.getElementById('numeroRifa').value;
+    if(nm==""){console.log("Digite o nome")}else{
+    nome[nr]=nm;
+    console.log(nome[nr]);
+    localStorage.setItem('nome', JSON.stringify(nome));
+   
+    };
+}
+
+function consulta(n) {
+   n=document.getElementById('consultaRifa').value;
+    ng = localStorage.getItem('nome');
+    document.getElementById('nomec').innerHTML = nome[n];
+    console.log(nome[n]);
+    console.log(ng);
+}
+
+function consultatd(tds){
+   t= tds.innerHTML;
+    console.log(t);
+    document.getElementById('nomec').innerHTML = nome[t];
+    
+}
+
+function elimiNull(){
+    let i = 0;
+    let k = 0;
+    nrv=0;
+   
+    
+    while(i < 102) {
+        
+        i++;
+        if(nome[i] == null) {}
+        else {
+            nrv++;
+            nomev[nrv] = nome[i];
+            console.log(nome[i]);
+            console.log(nomev[nrv]);
+        }
+
+        k++;
+        if(k > 102) { 
+            console.log("break");
+            break;
+        }
+    }
+    localStorage.setItem('nomev', JSON.stringify(nomev));
+    console.log(nomev);
+
+}
+
+
+
+
+//usar uma função com for para compilar todos os numeros em um array menor onde será feito o sorteio e depois que o sorteio for feito usar o indexOf no array principal para descobrir o número escolhidos pelo ganhador
