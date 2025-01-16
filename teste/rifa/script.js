32299993e54c6fc4913da2eea4a7c0f776179465
Allan <162851663+githuballan@users.noreply.github.com>
2025-01-16 14:54:35 -0300

    Rifa Concluída

--- teste/rifa/script.js
+++ teste/rifa/script.js
@@ -1,123 +1,143 @@
 var nome = [];
-var nr = 0;
-var nrv=0;
-var nomev = [];
-var inde = 0;
 
 onload = function () { 
-    nomev = JSON.parse(localStorage.getItem('nomev')) || [];
+    // nomev = JSON.parse(localStorage.getItem('nomev')) || [];
     nome = JSON.parse(localStorage.getItem('nome')) || [];
     console.log(nome);
-    console.log(nomev);
+    // console.log(nomev);
     marcar();
 }
 
-var cont=0;
+
 
 function sorteio() {
-    dis=document.getElementById('res');
-    console.log(dis);
-    dis.style.display = "block";
+    let ns=0;
+    let inscritos=0;
+    let nomesort=[];
+    let n=0;
+    let sorteio=0;
+    let k =0;
+    let inde=0;
 
-    if(nome.length==0){console.log("Rifa vazia")};
-
-    var n = Math.floor(Math.random() * nomev.length);
-    inde= nome.indexOf(nomev[n]);
-    if(n==0){n++;}
- 
-    console.log(inde);
-   console.log(nomev[n]);
-    console.log(n);
-    document.getElementById('nomes').innerHTML ="Parabéns "+ nomev[n]+"<br>" + "O número sorteado foi: "+inde;
-    
+    while(n<101){//verificar qtd de inscritos
+        n++;
+        if(nome[n]==null){}
+            else{
+                ns++
+                nomesort[ns]=nome[n];
+                inscritos++;            
+            }
+    }
    
-    
-}
-
-function add() {
-    if(document.getElementById('numeroRifa').value>100){console.log("Número inválido")}else{
-    var nm = document.getElementById('nomeRifa').value;
-    let nr = document.getElementById('numeroRifa').value;
-    if(nm==""){console.log("Digite o nome")}else{
-    nome[nr]=nm;
-    console.log(nome[nr]);
-    localStorage.setItem('nome', JSON.stringify(nome));
-
-   
-
-    document.getElementById('nomeRifa').value = "";
-    document.getElementById('numeroRifa').value = "";
-   
-    };
-};
-}
-
-function consulta(n) {
-   n=document.getElementById('consultaRifa').value;
-    ng = localStorage.getItem('nome');
-    document.getElementById('nomec').innerHTML = nome[n];
-    console.log(nome[n]);
-    console.log(ng);
-}
-
-function consultatd(tds){
-   t= tds.innerHTML;
-    console.log(t);
-    if(nome[t]==null){document.getElementById('nomec').innerHTML = "Vazio";
-        document.getElementById('numeroRifa').value = t;
-    }else{
-    document.getElementById('nomec').innerHTML = nome[t];
-    document.getElementById('numeroRifa').value = "";
-}
-    
-}
-
-function elimiNull(){
-   
-   nomev=nome;
-   console.log(nomev);
-   console.log(nome);
-
-    let i = 0;
-    let k = 0;
-    nrv=0;
-    
-    while(i < 101) {        
-        i++;
-        if(nome[i] == null) {}
-        else {
-            nrv++;
-            nomev[nrv] = nome[i];
-           
-        }
+    sorteio=101;// forçar entrar no loop while abaixo
+    while(sorteio>inscritos){//loop criado para sortear somente número já comprados
+       
+        sorteio = Math.floor(Math.random()*100);
+        if(sorteio<1){
+            sorteio = Math.floor(Math.random()*100);};
 
         k++;
-        if(k > 102) { 
+        if(k > 1000) { 
             console.log("break");
             break;
         }
-        localStorage.setItem('nomev', JSON.stringify(nomev));
     }
 
-    
-    
+    inde= nome.indexOf(nomesort[sorteio]);
+
+    console.log("nr sorteio "+sorteio);
+    console.log("nome do ganhador: "+nomesort[sorteio]);
+    console.log("nr escolhido pelo ganhador: "+inde);
+
+    dis=document.getElementById('res');
+    dis.style.display = "block";
+
+    document.getElementById('nomes').innerHTML ="Parabéns "+ nomesort[sorteio]+"<br>" + "O número sorteado foi: "+inde;
 
 }
 
+
+
+
+function Cadastrar() {
+        if(document.getElementById('numeroRifa').value>100){console.log("Número inválido")}
+        else{
+            var nm = document.getElementById('nomeRifa').value;
+            let nr = document.getElementById('numeroRifa').value;
+            if(nm==""){
+                console.log("Digite o nome")}
+                else{
+                    nome[nr]=nm;
+                    console.log(nome[nr]);
+                    localStorage.setItem('nome', JSON.stringify(nome));
+
+    
+
+                    document.getElementById('nomeRifa').value = "";
+                    document.getElementById('numeroRifa').value = "";
+                    document.getElementById('nomec').value = "";
+   
+                };
+            };
+marcar();
+
+}
+var t=0;
+
+function excluir(){
+        let td=0;
+
+        td=document.getElementById("n"+t)
+        td.removeAttribute('class','marcado')
+
+        console.log(td);
+        nome.splice(t,1,null);
+       
+        localStorage.setItem('nome', JSON.stringify(nome));
+        
+        dis=document.getElementById('Apagar');
+        dis.style.display = "none";
+        document.getElementById('nomec').value = "";
+
+    }
+
+
+function consultatd(tds){
+   t= tds.innerHTML;
+    if(nome[t]==null){document.getElementById('nomec').innerHTML = "Vazio";
+        document.getElementById('numeroRifa').value = t;
+
+        dis=document.getElementById('Apagar');
+        dis.style.display = "none";
+    
+    }else{
+    document.getElementById('nomec').innerHTML = nome[t];
+    document.getElementById('numeroRifa').value = "";
+    
+    dis=document.getElementById('Apagar');
+    dis.style.display = "block";
+
+
+    }
+    
+}
+
+
+
 function marcar(){
     let n = 0;
-    inde= nome.indexOf(nomev[n]);
+    let inde= nome.indexOf(nome[n]);
     let mar=document.getElementById('n'+inde);
 
     while(n < 102) {
         n++;
 
-        if(nomev[n] == null) {}
+        if(nome[n] == null) {}
         else {
-            inde= nome.indexOf(nomev[n]);
+            inde= nome.indexOf(nome[n]);
             mar=document.getElementById('n'+inde);
             mar.setAttribute('class','marcado');
-            console.log(nomev[n]);
+            console.log(nome[n]);
             console.log(inde);
          
         }
@@ -127,41 +147,46 @@ function marcar(){
 
    function apagMemoria(){
     console.log(nome);
-    console.log(nomev);
+    // console.log(nomev);
     let i = 0;
 
     while(i < 101) {
         i++;
         nome.shift();
-        nomev.shift();
+        // nomev.shift();
     }
     localStorage.clear();
-    
+
+    console.log(nome);
+    // console.log(nomev);   
 
    }
 
    function listar(){
-    
+    let bk=0;
     let n = 0;
-    let l = 0;
     let list = document.getElementById('list');
      list.innerHTML = "";//se clicar mais de uma vez não duplica a lista
-    let ul = document.createElement('ul');
-    list.appendChild(ul);
+     
+    console.log("clIcado");
     
-    while(n < 102) {
-        n++;
-        if(nomev[n] == null) {}
-        else {
-            inde= nome.indexOf(nomev[n]);
-            let li = document.createElement('li');
-            ul.appendChild(li);
-            li.innerHTML = nomev[n] + " "+inde;
-            l++;
+    while(n < 100) {n++;
+        if(nome[n] == null) {console.log(n);
+            bk++
+            if(bk == 100) { 
+            list.innerHTML=("<p>Não há inscritos</p>");
+            console.log(bk);
+            
+            
         }
-        if(l > 100) { 
-            console.log("break");
-            break;
         }
+        else { 
+            inde= nome.indexOf(nome[n]);
+            list.innerHTML += ("<li>"+nome[n]+": "+ inde+"</li>")
+        
+        
+       
+       
+    }
     }
    }
