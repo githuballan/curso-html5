2ccdf3c10982da145a08317935208865a575fb48
Allan <162851663+githuballan@users.noreply.github.com>
2025-02-13 16:43:15 -0300

    Atualizações
    Modelo de caixas atualizados
    incorporado menu de navegação de forma responsiva

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ function exibir(x){
         {k.style.display = "none";}
         else{k.style.display = "block";}
 };
+
+// function navload(){
+   
+//     fetch('../../index.html')
+//         .then(response => response.text())
+//         .then(data => {
+//             let parser = new DOMParser();
+//             let doc = parser.parseFromString(data, 'text/html');
+//             let content = doc.querySelector('#index-tipo'); // Adjust the selector as needed
+//             console.log(content);
+//             document.querySelector('#inde').innerHTML = content.innerHTML;
+//         })
+//         .catch(error => console.error('Error fetching the page:', error));
+//    }
+
+function toggleMenu(x) {
+    x.classList.toggle("change");
+}
