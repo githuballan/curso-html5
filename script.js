f9e9d9b3cf2561b53c152da3d5f3f6ea0fe44d43
Allan <162851663+githuballan@users.noreply.github.com>
2025-03-01 15:39:08 -0300

    Atualizações
    Adicionda a função createIndice para facilitar o caminho para a página.
    adicionada a função vídeos via iframe  no módulos 3 e 2.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var itemArray = [];
+
+
+
 function exibir(x){
    let k = x.nextElementSibling;
     
@@ -37,10 +41,9 @@ function valueMaxMin(max,min,input){
 
 function iframeload(x){
     let endereco = x.getAttribute('data-video-src');
-    let iframe=document.getElementsByTagName('iframe')[0];
+    iframe=document.getElementById('iframev');
     iframe.src = endereco;
-    console.log(endereco);
-    console.log(iframe);
+   
 }
 
 
@@ -48,7 +51,7 @@ function iframeload(x){
 function createLinkToVideos() {
 
   const navVideos= document.getElementById('nav_videos');
- 
+ if(navVideos!==null){
 navVideos.innerHTML = `
 <img src="../../icones/YouTube_icon_(2013-2017).png" alt="Play" id="botao_menu-videos" class="buttom-videos" onclick="exibir(this)">
 
@@ -57,8 +60,8 @@ navVideos.innerHTML = `
 
   // Seleciona todos os elementos <li> dentro da <ul>
   const videoList = document.getElementById('video_list');
- let item = videoList.getElementsByTagName('li');
- let itemArray = [...item];
+  let item = videoList.getElementsByTagName('li');
+  var itemArray = [...item];
  
 
   var navList = document.getElementById('lista_videos');
@@ -69,12 +72,14 @@ navVideos.innerHTML = `
      
       newLi.innerHTML = `<ins class="link-vid-sqn" onclick="clicador('botao_menu-videos'); iframeload(this)" data-video-src="${item.getAttribute('data-adress')}">${item.textContent}</ins>`;
       navList.appendChild(newLi);
-    
   });
-}
+  // iframeload
+  iframeload(navList.firstElementChild.firstElementChild);
+}}
 
 function createMenuAcessoRapido(){
   var MenuAcessoRapido=document.getElementById('menu_acesso_rapido');
+  if(MenuAcessoRapido!==null){
   MenuAcessoRapido.innerHTML = `<button id="menu-hamburguer" onclick="toggleMenu(this); exibir(this)">
         <div class="bar1"></div>
         <div class="bar2"></div>
@@ -85,6 +90,7 @@ function createMenuAcessoRapido(){
     
     
     const itemShortcutsName = document.getElementById('shortcuts');
+
     var itemShortcutsNameArray = [...itemShortcutsName.getElementsByTagName('li')];
    
     let shortcutsList = document.getElementById('shortcuts_list');
@@ -97,25 +103,84 @@ function createMenuAcessoRapido(){
         shortcutsList.appendChild(newLi);
       
     });
+}}
+ 
+function createIndice(h){
+    let nextElementIrmão = h.nextElementSibling;
+    
+    // Verifica se os links já foram criados
+    if (nextElementIrmão.getAttribute('data-links-created') === 'true') {
+      return; // Sai da função se os links já foram criados
+    }
+  
+    // Marca que os links foram criados
+    nextElementIrmão.setAttribute('data-links-created', 'true');
+
+    // Início da criação dos links
+  const itemShortcutsName = nextElementIrmão.firstElementChild;
+
+  let wayHref=h.firstElementChild.getAttribute('href');
+
+  console.log(nextElementIrmão.firstElementChild);
+
+  var itemShortcutsNameArray = [...itemShortcutsName.getElementsByTagName('li')];
+ 
+  let shortcutsList = nextElementIrmão;
+  
+  console.log(itemShortcutsNameArray);
+  
+
+  // Itera sobre cada elemento <li> e faz algo com ele
+  itemShortcutsNameArray.forEach((item) => {
+      let newLi = document.createElement('li');
+      newLi.innerHTML = `<a href="${wayHref}${item.getAttribute('data-adress')}">${item.textContent}</a>`;
+      shortcutsList.appendChild(newLi);
+    
+  });
+  console.log(shortcutsList);
+  //if(shortcutsList.style.display === 'none'){shortcutsList.innerHTML='';}//não dá certo pois ele apaga a ul com os li data-adress então depois fica sem ter de onde pegar informação para criar o indice
 }
 
-function createIndexButton(){
-  document.getElementById('button_index').innerHTML = `<a class="buttom buttom-index" href="../../index.html">Índice</a>`;
 
-  
+function createIndexButton(){
+  let index = document.getElementById('button_index')
+  if(index!==null){
+  index.innerHTML = `<a class="buttom buttom-index" href="../../index.html">Índice</a>`;  
+}
 }
 
 function createTelaVideo(){
-  document.getElementById('video_iframe').innerHTML = `
+  const configIframe = document.getElementById('config_iframe');
+  if(configIframe!==null){
+  let tela=document.createElement('div');
+  tela.id = 'tela_iframe';
+  configIframe.appendChild(tela);
+  tela.innerHTML = `<iframe id="iframev" width="867" height="490" src="https://www.youtube.com/embed/1QTi8nIlK1o" title="Criando conta no GitHub - @Curso em Vídeo HTML5 e CSS3" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+  let controle=document.createElement('div');
+  controle.id = 'controles_iframe';
+  configIframe.appendChild(controle);
+  controle.innerHTML = `
+  <button  id="botao_fixa_solta" onclick="toggleIframePosition(this)">Fixar</button>
+
+    <input itemid="range_video" style="width:calc(100% - 100px); display: inline-block" type="range" id="range_video" value="100" min="30" max="100" oninput="document.getElementById('config_iframe').style.width=this.value+'%'">
+
+    <img id="pip" src="../../imagens/pip.svg"onclick="botaoPip()" alt="botão pip
+   ">
+    
+    <img id="pipout" src="../../imagens/pipout.svg" onclick="botaoGetOutPip()" alt="botão pipout">
+</div>
   `;
-};//Lembrar de colocar o src do iframe igual ao primeiro video da lista usando o array de videos
+//Lembrar de colocar o src do iframe igual ao primeiro video da lista usando o array de videos
+
+}}
 
 
 window.addEventListener('load', () => {
   createIndexButton();
-  // createTelaVideo();
+  createTelaVideo();
   createLinkToVideos();
   createMenuAcessoRapido();
+  
 });
 
 
@@ -170,6 +235,3 @@ function toggleIframePosition(button) {
     button.innerHTML = 'Soltar';
   }
 }
-
-       
-  
